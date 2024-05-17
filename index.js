3dda6677f59807599854ae7a95157577f7e1020b
Javier Godoy <javiergodoymiguez@gmail.com>
2024-05-17 21:09:35 +0200

    finally solved

--- index.js
+++ index.js
@@ -17,6 +17,34 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
+    // Add a new recipe to the database
+    const newRecipe = {
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup rice vinegar",
+        "5 tablespoons honey",
+        "1/3 cup soy sauce (such as Silver Swan®)",
+        "1/4 cup Asian (toasted) sesame oil",
+        "3 tablespoons Asian chili garlic sauce",
+        "3 tablespoons minced garlic",
+        "salt to taste",
+        "8 skinless, boneless chicken thighs"
+      ],
+      cuisine: "Asian",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Chef LePapu"
+    };
+
+    return Recipe.create(newRecipe);
+  })
+  .then(recipe => {
+    // Log the title of the newly inserted recipe
+    console.log(`Recipe created: ${recipe.title}`);
+  })
+  .then(() => {
 
     return Recipe.insertMany(data);
   })
