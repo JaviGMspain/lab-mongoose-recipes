1fc3fa9631417460517835b9b86967cbb693c117
Javier Godoy <javiergodoymiguez@gmail.com>
2024-05-17 21:01:24 +0200

    solved lab

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+mongoose.set(`strictQuery`, true);
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,7 +17,35 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+    return Recipe.insertMany(data);
+  })
+  .then(recipes => {
+    recipes.forEach(recipe => console.log(`Recipe created: ${recipe.title}`));
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then(updatedRecipe => {
+    if (updatedRecipe) {
+      console.log(`Succesfully updated duration for ${updatedRecipe.title} to ${updatedRecipe.duration} minutes`);
+    } else {
+      console.log(`recipe not found`);
+    }
+    return Recipe.deleteOne({title: "Carrot Cake"});
+  })
+  .then(deleteResult => {
+    if (deleteResult.deletedCount > 0) {
+      console.log(`Successfully deleted the "carrot Cake" recipe`);
+    } else {
+      console.log(`Recipe "Carrot Cake" not found`);
+    }
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log(`Database connection closed`);
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
